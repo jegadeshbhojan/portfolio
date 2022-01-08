d05774418c27b18d9e19d76ae3cb8b24e56312e3
jegadeshbhojan <jegadeshin@gmail.com>
2022-01-09 00:42:45 +0530

    check CD

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -3,8 +3,8 @@ export function Skills (){
     return(
         <div className="Skills">
         <h1>Technologies</h1>
-        <h3>I am web  developer developing web applications using Technologies like   </h3>
-        <ul>
+        <h3 className="heading">I am a web  developer developing web applications using Technologies like   </h3>
+        <ul className="techlist">
             <li>React js</li>
             <li>Redux</li>
             <li>Javascript</li>
@@ -12,6 +12,7 @@ export function Skills (){
             <li>Node js</li>
             <li>css</li>
             <li>jest </li>
+            <li>Bootstarp</li>
         </ul>
         </div>
     )
